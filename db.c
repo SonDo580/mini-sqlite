eb07b81b8b6fba445e59fcbcaf26460c28a9e9d0
Son Do <dominhson580@gmail.com>
2025-12-20 00:38:36 +0700

    skeleton - parse and execute commands

--- db.c
+++ db.c
@@ -6,13 +6,36 @@
 #include <string.h>
 #include <sys/types.h>
 
-/* To interact with `getline`. */
+/* Wrapper to interact with `getline`. */
 typedef struct
 {
     char *buffer;         // buffer containing the read line
     size_t buffer_length; // size of allocated buffer
     ssize_t input_length; // number of bytes read
 } InputBuffer;
+
+typedef enum
+{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum
+{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum
+{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT,
+} StatementType;
+
+typedef struct
+{
+    StatementType type;
+} Statement;
 
 InputBuffer *new_input_buffer()
 {
@@ -24,13 +47,11 @@ InputBuffer *new_input_buffer()
     return input_buffer;
 }
 
-/* Print a prompt to user. */
 void print_prompt()
 {
     printf("db > ");
 }
 
-/* Read a line of input. */
 void read_input(InputBuffer *input_buffer)
 {
     ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
@@ -46,12 +67,52 @@ void read_input(InputBuffer *input_buffer)
     input_buffer->buffer[bytes_read - 1] = 0; // null terminator '\0'
 }
 
-/* Free memory allocated for an InputBuffer instance. */
 void close_input_buffer(InputBuffer *input_buffer)
 {
-    // `getline` allocates memory for input_buffer->buffer in `read_input`
     free(input_buffer->buffer);
     free(input_buffer);
+}
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+    switch (statement->type)
+    {
+    case STATEMENT_INSERT:
+        printf("Perform insert.\n");
+        break;
+    case STATEMENT_SELECT:
+        printf("Perform select.\n");
+        break;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -62,14 +123,30 @@ int main(int argc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        if (input_buffer->buffer[0] == '.')
         {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+            // Handle meta commands (non-SQL statements)
+            switch (do_meta_command(input_buffer))
+            {
+            case META_COMMAND_SUCCESS:
+                continue;
+            case META_COMMAND_UNRECOGNIZED_COMMAND:
+                printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                continue;
+            }
         }
-        else
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
         {
-            printf("Unrecognized command '%s.\n", input_buffer->buffer);
+        case PREPARE_SUCCESS:
+            break;
+        case PREPARE_UNRECOGNIZED_STATEMENT:
+            printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+            continue;
         }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
 }
